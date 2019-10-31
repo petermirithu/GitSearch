2e1ef76015b764252b31b9448ea7ff49d9a442df
petermirithu <pyra_m.k@yahoo.com>
2019-10-31 21:12:42 +0300

    create a parent component Gitsearch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitsearchComponent } from './gitsearch/gitsearch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitsearchComponent
   ],
   imports: [
     BrowserModule,
